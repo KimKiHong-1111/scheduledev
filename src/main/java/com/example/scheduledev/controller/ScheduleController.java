ed6a48905f954eefb990a8ee8f68c55f7671c778
Kihong_Kim <rlghd0157gmail.compwd>
2025-02-07 17:22:23 +0900

    오타수정

--- src/main/java/com/example/scheduledev/controller/ScheduleController.java
+++ src/main/java/com/example/scheduledev/controller/ScheduleController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/boards")
+@RequestMapping("/schedules")
 @RequiredArgsConstructor
-public class BoardController {
+public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
